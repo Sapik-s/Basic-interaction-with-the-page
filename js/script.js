08e447d854a18f89e16fbb000f20ca98243542ce
Sapik <Sapik_19@mail.ru>
2022-06-22 16:21:14 +0300

    Entering data into an input/Adding to an object/Output to the screen/Delete by button/Add to Favorites/Warning about incorrect data entry

--- js/script.js
+++ js/script.js
@@ -1,69 +1,110 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта) +
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Скотт Пилигрим против...",
-        "Одержимость",
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд"
-    ]
-};
-//Функция удаления рекламных баннеров
-//-----------------------------------------------------
-function deleteAdvFunc(){
-    const deleteAdv = document.querySelectorAll('.promo__adv img');
+document.addEventListener('DOMContentLoaded', () => {
+    const movieList = document.querySelector('.promo__interactive-list'),
+    deleteAdv = document.querySelectorAll('.promo__adv img');
+    const movieDB = {
+        movies: [
+            "Скотт Пилигрим против...",
+            "Одержимость",
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+        ]
+    };
+    //Функция удаления рекламных баннеров
+    //-----------------------------------------------------
+    function deleteAdvFunc(del){
+        del.forEach(element => {
+            element.remove();
+        });
+    }
+    deleteAdvFunc(deleteAdv);
 
-    deleteAdv.forEach(element => {
-        element.remove();
-    });
-}
-
-//Функция замены жанра
-//-----------------------------------------------------
-function replaceGenreFunc() {
-    const replaceGenre = document.getElementsByClassName('promo__genre');
+    //Функция замены жанра
+    //-----------------------------------------------------
+    function replaceGenreFunc() {
+        const replaceGenre = document.getElementsByClassName('promo__genre');
+        
+        replaceGenre[0].innerHTML = 'ДРАМА';
+    }
     
-    replaceGenre[0].innerHTML = 'ДРАМА';
-}
+    //Функция замены заднего фона
+    //-----------------------------------------------------
+    function replaceBgFunc() {
+        const replaceBg = document.getElementsByClassName('promo__bg');
+    
+        replaceBg[0].style.cssText = `background: url('../img/bg.jpg');`;
+    }
+    
+    //Функция формирование списка фильмов
+    //-----------------------------------------------------
+    function creatingListMovies(films, parent) {
+        parent.innerHTML = "";
+        sortArr(films);
+    
+        films.forEach((film, i) => {
+            parent.innerHTML += `
+                <li class="promo__interactive-item">${i + 1} ${film}
+                    <div class="delete"></div>
+                </li>
+            `;
+        });
 
-//Функция замены заднего фона
-//-----------------------------------------------------
-function replaceBgFunc() {
-    const replaceBg = document.getElementsByClassName('promo__bg');
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+                
+                creatingListMovies(movieDB.movies, movieList);
+            });
+        });
+    }
 
-    replaceBg[0].style.cssText = `background: url('../img/bg.jpg');`;
-}
+    function sortArr(arr) {
+        arr.sort();    
+    }
 
-//Функция формирование списка фильмов
-//-----------------------------------------------------
-function creatingListMovies() {
-    const getListMovies = document.querySelectorAll('.promo__interactive-item');
+    //Запуск функций
+    //-----------------------------------------------------
+        replaceGenreFunc();
+        replaceBgFunc();
+        creatingListMovies(movieDB.movies, movieList);
+    //-----------------------------------------------------
+    
+    
+    const addForm = document.querySelector('form.add'),
+    addInput = addForm.querySelector('.adding__input'),
+    checkbox = addForm.querySelector('[type="checkbox"]'),
+    deleteMovies = addForm.querySelector('.delete');
+    
+    addForm.addEventListener('submit', (e) => {
 
-    getListMovies.forEach((element, i) => {
-        element.innerHTML = movieDB.movies[i];
+        e.preventDefault();
+
+        let newFilm = addInput.value,
+        favourite = checkbox.checked;
+
+        if (newFilm !== "" && newFilm.length > 2) {
+            movieDB.movies.push(newFilm);
+            creatingListMovies(movieDB.movies, movieList);
+
+            addInput.style.cssText = `border: 1px solid rgba(0, 0, 0, 0.19);`;
+            addInput.placeholder = "Что уже посмотрено...?";
+
+            e.target.reset();
+
+            if (newFilm > 21) {
+                newFilm = `${newFilm.substring(0, 22)}...`;
+            }
+
+        } else {
+            addInput.style.cssText = `border-bottom: 2px solid red`;
+            addInput.placeholder = "Мин кол-во символов 3";
+        }
+
+        if (favourite == true) {
+            console.log(`Добавляем любимый фильм ${newFilm}`);
+        }
     });
-}
-movieDB.movies.sort();
-
-//Запуск функций
-//-----------------------------------------------------
-deleteAdvFunc();
-replaceGenreFunc();
-replaceBgFunc();
-creatingListMovies();
-//-----------------------------------------------------
+});
